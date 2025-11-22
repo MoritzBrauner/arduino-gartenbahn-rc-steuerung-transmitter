de54e73c1b8b5afb628a39fd347508412c874044
Moritz Brauner <brauner.moritz@googlemail.com>
2025-11-22 19:56:09 +0100

    Vorbereitungen Mapping functionality

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define DEFAULT_VALUE 512 //Stanardwert für Min/Max auslesen 
+
 //Radio - Pins NRF24 
 RF24 radio(7, 8); // CE, CSN
 
@@ -21,21 +23,20 @@ struct Data_Package {
 //Datenpacket als "data" festlegen
 Data_Package data;
 
-
 //Initializer to find the physical limits of the analogue joysticks of the remote
 struct Min_Values {
-  int lx = 0; 
-  int ly = 0; 
-  int rx = 0; 
-  int ry = 0; 
+  int lx = DEFAULT_VALUE; 
+  int ly = DEFAULT_VALUE; 
+  int rx = DEFAULT_VALUE; 
+  int ry = DEFAULT_VALUE; 
 };
 Min_Values minValues; 
 
 struct Max_Values {
-  int lx = 0; 
-  int ly = 0; 
-  int rx = 0; 
-  int ry = 0; 
+  int lx = DEFAULT_VALUE; 
+  int ly = DEFAULT_VALUE; 
+  int rx = DEFAULT_VALUE; 
+  int ry = DEFAULT_VALUE; 
 };
 Max_Values maxValues; 
 
@@ -70,6 +71,15 @@ void loop() {
   data.rx = analogRead(3);
   data.ry = analogRead(4);
   data.rz = digitalRead(A5);
+
+  //Min und max setzen  
+  minValues.lx = min(data.lx, minValues.lx); 
+  minValues.rx = min(data.ly, minValues.ly); 
+
+
+  //Analoge Reads den min / max-werten zumappen 
+
+
 
   //Daten senden 
   radio.write(&data, sizeof(Data_Package));
